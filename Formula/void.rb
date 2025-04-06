4a68947875ff2d61f815a39426fcf0bc8c3d6083
noahniemeijer <noahniemeijer0603@gmail.com>
2025-04-06 21:23:56 +0200

    Test12

--- Formula/void.rb
+++ Formula/void.rb
@@ -1,24 +1,21 @@
 class Void < Formula
-  desc "VOID Program"
+  desc "Void CLI Program"
   homepage "https://github.com/noahniemeijer/void"
   url "https://github.com/noahniemeijer/VOID/archive/refs/tags/BETA.tar.gz"
-  version "BETA"
-  sha256 "a932245a708b92482dd5849d116bb5387decca5c048a93d848155a02ecedceed"
+  sha256 "d8008b343d86cafdc7682abf644bddf657c44057024a931041330b0fea95f939"
 
   def install
-    # Install the main.py file into libexec
-    libexec.install "main.py"
+    # Install the main.py script into libexec
+    libexec.install "libexec/main.py"
     
-    # Create a symlink to main.py in /usr/local/bin, so it's accessible as 'void'
-    bin.write_exec_script libexec/"main.py"
+    # Create a wrapper script for void in bin
+    (bin/"void").write <<~EOS
+      #!/bin/bash
+      python3 #{libexec}/main.py "$@"
+    EOS
   end
 
-  def caveats
-    <<~EOS
-      Run the app with:
-        void
-
-      Ensure ollama is installed and accessible in your path.
-    EOS
+  test do
+    system "#{bin}/void", "--help"
   end
 end
