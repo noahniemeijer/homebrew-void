a213c6053127c72247c7cbbc6fe6c61a466cf252
noahniemeijer <noahniemeijer0603@gmail.com>
2025-04-06 21:13:18 +0200

    Test11

--- Formula/void.rb
+++ Formula/void.rb
@@ -1,12 +1,15 @@
 class Void < Formula
-  desc "My Project Description"
+  desc "VOID Program"
   homepage "https://github.com/noahniemeijer/void"
   url "https://github.com/noahniemeijer/VOID/archive/refs/tags/BETA.tar.gz"
   version "BETA"
   sha256 "a932245a708b92482dd5849d116bb5387decca5c048a93d848155a02ecedceed"
 
   def install
+    # Install the main.py file into libexec
     libexec.install "main.py"
+    
+    # Create a symlink to main.py in /usr/local/bin, so it's accessible as 'void'
     bin.write_exec_script libexec/"main.py"
   end
 
