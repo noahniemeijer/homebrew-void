74e40a764601af22274e4bb6cdd509d66ce0de85
noahniemeijer <noahniemeijer0603@gmail.com>
2025-04-06 20:47:29 +0200

    Test5

--- Formula/void.rb
+++ Formula/void.rb
@@ -1,8 +1,8 @@
 class Void < Formula
   desc "VOID – A terminal AI interface"
   homepage "https://github.com/noahniemeijer/void"
-  url "https://github.com/noahniemeijer/void/archive/refs/tags/VOID-BETA.tar.gz"
-  version "VOID-BETA"
+  url "https://github.com/noahniemeijer/VOID/archive/refs/tags/BETA.tar.gz"
+  version "BETA"
   sha256 "d8008b343d86cafdc7682abf644bddf657c44057024a931041330b0fea95f939"
 
   depends_on "python@3.12"
